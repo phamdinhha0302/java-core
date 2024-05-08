7514fa36e85ebe1a2a961d5bcec042e680363b63
phamdinhha0302 <phamdinhhahtka@gmail.com>
2024-05-08 17:03:19 +0700

    as2

---
+++ src/main/java/Exam.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Exam {
+    int id;
+    String code;
+    String title;
+    CategoryQuestion category;
+    int duration;
+    Account creator;
+    LocalDate createdDate;
+}
