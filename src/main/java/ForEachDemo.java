f217bcd8bfae09add839760b10c666442ef41a4c
phamdinhha0302 <phamdinhhahtka@gmail.com>
2024-05-17 08:31:26 +0700

    them

--- src/main/java/ForEachDemo.java
+++ src/main/java/ForEachDemo.java
@@ -1,6 +1,7 @@
 public class ForEachDemo {
     public static void main(String[] args) {
         int [] numbers = { 3,4,5,6 };
+        // number tượng trung cho các biến trong mảng
         for ( int number : numbers){
             System.out.println("number = "+number);
         }
